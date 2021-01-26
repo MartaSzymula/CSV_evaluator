c03effaae089077b55b4e09ee65bbca77d7ceb2b
Marta Szymula <martaszymula2@gmail.com>
2021-01-26 22:48:23 +0100

    minor update

--- CSV_validation/settings.py
+++ CSV_validation/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a^-jg2(!!qkzzcr2li3t(bd&350gyw2z-mw9nz=+=q-s94eb!s'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
